90c96931e63ce312d0ce42e2a8e0c3f9d528b64f
Abhishek Kathuria <abhishekkathuria40@gmail.com>
2020-06-09 16:26:53 +0530

    preprocess human3.6 dataset

--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,29 @@
+
+# importing all the libraries 
+#also importing the files and labels for human 3.6  from data folder
 import os
 import sys
 import argparse
 import csv
 import numpy as np
 import h5py
+from data.utils import *
+from data.format.human36m.body import BodyFileReader, get_labels
 
-from braniac.utils import *
-from braniac.format.human36m.body import BodyFileReader, get_labels
 
 # A class to split the training data S1-S11 and generate a CSV file
-class DataSplitter(object):
+class Data_split_human36(object):
     def __init__(self, input_folder):
 
     #Here, the input_folder contains the path of the folder where all the clips are stored
-        self._input_folder = input_folder
+        self._input_folder_human36 = input_folder
         self._items = []
         self._stats_context = DataStatisticsContext() # it is a class which gives the pointer to the data statistics
 
+
 # A function to load all the files as a python list including the labesls as well
 
-    def load_data_paths(self):
+    def load_data_human36(self):
         
         labels = get_labels() # to obtain the labels
         sub_folders = os.listdir(self._input_folder)
@@ -45,12 +49,11 @@ class DataSplitter(object):
                 if (index % 100) == 0:
                     print("Process {} items.".format(index+1))
                 index += 1
-        
         return self._items
 
 # A boolean function to add item frames
 
-    def _filter_data(self, item_path):
+    def filter_human36(self, item_path):
        
                frames = BodyFileReader(item_path)
         if len(frames) >= 60:
@@ -60,27 +63,29 @@ class DataSplitter(object):
             return True
         return False
 
-    def split_data(self, items):
+    
+
+#splitting the data into training and testing data
+#here, items is collection of clips and their label
+
+    def split_data_human36(self, items):
         
         item_count = len(items)
-        indices = np.arange(item_count)
+        indices = np.arange(item_count) #create a numpy array with the lengh = the no. of items
         np.random.shuffle(indices)
-
         train_count = int(0.8 * item_count)
         test_count  = item_count - train_count
-
         train = []
         test  = []
-
         for i in range(train_count):
             train.append(items[indices[i]])
-
         for i in range(train_count, train_count + test_count):
             test.append(items[indices[i]])
-
         return train, test
 
-    def write_to_csv(self, items, file_path):
+    
+
+    def write_csv_human36(self, items, file_path):
         
         if sys.version_info[0] < 3:
             with open(file_path, 'wb') as csv_file:
@@ -92,57 +97,53 @@ class DataSplitter(object):
                 writer = csv.writer(csv_file, delimiter=',')
                 for item in items:
                     writer.writerow(item)
+    
 
-    def compute_statistics(self):
-        
+    def compute_statistics_human36(self):
         with BodyDataStatisticsPass1(self._stats_context) as stats:
             for item in self._items:
                 frames = BodyFileReader(item[0])
                 for frame in frames:
                     stats.add(frame[0].as_numpy())
-
         with BodyDataStatisticsPass2(self._stats_context) as stats:
             for item in self._items:
                 frames = BodyFileReader(item[0])
                 for frame in frames:
                     stats.add(frame[0].as_numpy())
-
         return self._stats_context
 
+    
 def main(input_folder, output_folder):
+# it iterates over the given list of clips and labels, then converts them to train and test data
    
-    data_splitter = DataSplitter(input_folder)
-    items = data_splitter.load_data_paths()
-    print("{} items loaded, start splitting.".format(len(items)))
-
-    train, test = data_splitter.split_data(items)
-    print("Train: {} and test: {}.".format(len(train), len(test)))
-
-    context = data_splitter.compute_statistics()
-    print("Complete computing statistics.")
-
-    save_statistics_context(context, os.path.join(output_folder, 'data_statistics.h5'))
-
+    data_human36 = data_split_human36(input_folder)
+    items_human36 = data_split_human36.load_data_human36()
+    print("{} items loaded for human 3.6 dataset".format(train_human36, test_human36 = data_split_human36.split_data(items)
+    
+    context_human36 = data_split_human36.compute_statistics_human36()
+    #print("Complete computing statistics.")
     if len(train) > 0:
-        data_splitter.write_to_csv(train, os.path.join(output_folder, 'train_map.csv'))
+        data_split_human36.write_csv_human36(train, os.path.join(output_folder, 'train_human36.csv'))
+        data_split_ntu.write_csv_ntu(train, os.path.join(output_folder, 'train_ntu.csv'))
     if len(test) > 0:
-        data_splitter.write_to_csv(test, os.path.join(output_folder, 'test_map.csv'))
-
-    print("Done.")
+        data_split_human36.write_to_csv(test, os.path.join(output_folder, 'test_map.csv'))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i",
                         "--input_folder",
                         type = str,
-                        help = "Input folder containing the raw data.",
+                        help = "Input folder",
                         required = True)
 
     parser.add_argument("-o",
                         "--output_folder",
                         type = str,
-                        help = "Output folder for the generated training, validation and test text files.",
+                        help = "Output folder",
                         required = True)
 
     args = parser.parse_args()
     main(args.input_folder, args.output_folder)
+
+
+    
